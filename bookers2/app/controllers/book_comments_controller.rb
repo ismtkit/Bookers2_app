5f35d6079ac7463c2e9a2d00e594a18aef6dc0be
ismtkit <kit1590@icloud.com>
2021-05-10 14:38:41 +0000

    [finish]F2-6

--- bookers2/app/controllers/book_comments_controller.rb
+++ bookers2/app/controllers/book_comments_controller.rb
@@ -1,29 +1,25 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-     if comment.save
-        redirect_to book_path(book)
-     else
-        @book = Book.find(params[:book_id])
-        @book_new = Book.new
-        @user = @book.user
-        @book_comment = comment
-        render 'books/show'
-     end
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.build(params.require(:book_comment).permit(:comment))
+    @comment.book_id = @book.id
+    if @comment.save
+       @book_comment = BookComment.new
+    else
+      @book = Book.find(params[:book_id])
+      @book_new = Book.new
+      @user = @book.user
+      @book_comment = @comment
+      render 'error'
+    end
   end
 
   def destroy
+     @book = Book.find(params[:book_id])
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+     @book_comment = BookComment.new
   end
 
-  private
-
-  def book_comment_params
-    params.require(:book_comment).permit(:comment)
-  end
 
 end
